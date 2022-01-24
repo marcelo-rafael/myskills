581c90c96ad1b42b4fb144aacc742383a7e1e19d
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2022-01-23 21:44:14 -0300

    Tipando componente Button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -54,7 +54,7 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSkill} />
+      <Button title="Add" onPress={handleAddNewSkill} />
 
       <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
 
